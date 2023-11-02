4bca286e641d98bbe4e137689dde9f140e9aec6f
dishuarora <dikshaoibsip12@gmail.com>
2023-11-02 20:54:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const screen = document.getElementById("screen");
+const button = document.querySelectorAll("button");
+
+let string = "";
+
+button.forEach((number)=>
+{
+
+    number.addEventListener('click', (even)=>
+    {
+          let btnText =  even.target.innerText;
+          screen.value += btnText;
+
+
+          if(btnText == "AC")
+          {
+            string = "";
+            screen.value = string;
+          }
+          else if(btnText == "Sqrt")
+          {
+            string = Math.sqrt(string);
+            screen.value = string;
+          }
+
+          else if (btnText == "=")
+          {
+  string = eval(string)
+  screen.value = string;
+          }
+
+          else if (btnText == "C")
+          {
+            string = string.toString().slice(0,-1);
+            screen.value = string;
+          }
+          else{
+            string += btnText;
+            screen.value = string;
+          }
+
+    })
+})
